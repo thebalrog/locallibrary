35c56efc4c0be424902e69fac7c10bf91acfd57f
Chris Barlog <chrisbarlog@gmail.com>
2020-01-30 20:39:04 -0800

    uploading files to begin version control

---
+++ catalog/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render
+
+# Create your views here.
+from catalog.models import Book, Author, BookInstance, Genre
+from django.views import generic
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    
+    # The 'all()' is implied by default.    
+    num_authors = Author.objects.count()
+
+    num_a = Book.objects.filter(title__contains='a').count()
+    
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_visits': num_visits,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 1
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+	model = Author
+	paginate_by = 1
+
+from django.shortcuts import get_object_or_404
+
+class AuthorDetailView(generic.DetailView):
+	model = Author
+
